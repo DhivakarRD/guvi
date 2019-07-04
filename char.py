f0340a9e5415dcd3f6006d9b09d6bd72574f2cff
DhivakarRD <52446246+DhivakarRD@users.noreply.github.com>
2019-07-04 09:49:13 +0530

    Update char.py

--- char.py
+++ char.py
@@ -1,4 +1,4 @@
-ch = input("Enter the character: ")
+ch = input()
 
 if(ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u' or ch == 'A'or ch == 'E' or ch == 'I' or ch == 'O' or ch == 'U'):
   print('Vowel')
