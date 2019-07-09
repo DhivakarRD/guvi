e283867c9eef8e5c0d61cda3d1ce77b70d013dcd
DhivakarRD <52446246+DhivakarRD@users.noreply.github.com>
2019-07-09 14:11:03 +0530

    Update countprime.py

--- countprime.py
+++ countprime.py
@@ -1,12 +1,12 @@
 
-num1,num2=map(int,input().split())
-count=0
+numb1,numb2=map(int,input().split())
+ct=0
 for i in range(num1,num2+1):
     if(i > 1):
         for j in range(2,i):
             if((i%j)==0):
                 break
         else:
-            count=count+1
+            ct=ct+1
 
-print(count)
+print(ct)
