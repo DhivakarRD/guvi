ab5f5302ea12b50572dce489015b989cc186d605
DhivakarRD <52446246+DhivakarRD@users.noreply.github.com>
2019-07-09 19:44:24 +0530

    Create indexmatch.py

---
+++ indexmatch.py
@@ -0,0 +1,11 @@
+numb=int(input())
+lst=list(map(int,input().split()))
+temp=[]
+for i in range(numb):
+  if (lst[i]==i):
+    temp.append(str(lst[i]))
+temp.sort()
+if(len(temp)==0):
+    print("-1")
+else:
+    print(" ".join(temp))
