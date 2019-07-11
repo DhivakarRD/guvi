72da1d1aad075fad71f7abe7b7cac4ee887e038a
DhivakarRD <52446246+DhivakarRD@users.noreply.github.com>
2019-07-11 19:06:30 +0530

    Create commonprefix.py

---
+++ commonprefix.py
@@ -0,0 +1,12 @@
+numb=int(input())
+lst=[]
+for i in range(0,numb):
+   lst1=input()
+   lst.append(lst1)
+lst2=[]
+for i in zip(*lst):
+ if(i.count(i[0])==len(i)):
+  lst2.append(i[0])
+ else:
+  break
+print(''.join(lst2))
